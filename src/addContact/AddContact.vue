ac1b34c87f5e585d05b980a61fcbebcee05c7b82
Dmitriy Bukharev <needleinspace@gmail.com>
2020-11-12 15:31:19 +0500

    fix(kb): Исправления ключей, промисы в actions, фиксы форматирования, get методы вместо переменных класса

--- src/addContact/AddContact.vue
+++ src/addContact/AddContact.vue
@@ -1,116 +1,133 @@
 <template>
-    <div>
-        <div id = "main-layout">
-            <div id = "addContact-layout">
-                <div id = "addContact-header">
-                    <router-link to="/kb" class="nav-element" active-class="active">
-                        <div id = "return-button">
-                            <i class="fa fa-arrow-left" aria-hidden="true"></i>
-                        </div>
-                    </router-link>
-                    <div id = "contactName-field">
-                        <input v-model.lazy.trim = "contact.contactName" id = "contactName-input"
-                        placeholder = "ФИО" />
-                    </div>
-                </div>
-                <div id = "fields-layout">
-                    <div id = "companyId-layout" class = "contactField">
-                        <div id = "companyId" class = "fieldTitle">Компания</div>
-                        <select v-model = "contact.companyId" id = "companyVolumeContent">
-                            <option disabled value = 0>Выберите компанию</option>
-                            <option v-for = "company in companies" v-bind:key = "company"
-                            v-bind:value = "company.companyId">
-                                {{ company.fullName }}
-                            </option>
-                        </select>
-                    </div>
-                    <div id = "position-layout" class = "companyField">
-                        <div id = "position" class = "fieldTitle">Должность</div>
-                        <div class = "fieldBorder">
-                            <input v-model.lazy.trim = "contact.position"
-                            class = "fieldContent" placeholder = "Должность" />
-                        </div>
-                    </div>
-                    <div id = "makeDecision-layout" class = "contactField">
-                        <div id = "makeDecision" class = "fieldTitle">
-                            Лицо принимающее решения
-                        </div>
-                        <input v-model = "contact.makeDecision" type = "checkbox"
-                        class = "fieldContent" id = "makeDecision-checkbox"/>
-                    </div>
-                    <div id = "mainEmail-layout" class = "companyField">
-                        <div id = "mainEmail" class = "fieldTitle">Основной E-mail</div>
-                        <div class = "fieldBorder">
-                            <input v-model.lazy.trim = "contact.mainEmail"
-                            class = "fieldContent" placeholder = "Основной E-mail" />
-                        </div>
-                    </div>
-                    <button id = "addEmail-button" class = "addButton-layout"
-                    v-on:click = "addEmailButtonClick()">
-                        <div id = "addEmail-plus" class = "plus">+</div>
-                        <div id = "addEmail-text" class = "addButton-text">Добавить E-mail</div>
-                    </button>
-                    <ul class = "list">
-                        <li class = "listElement" v-for = "email in emails" v-bind:key = "email">
-                            <div class = "fieldBorder">
-                                <input class = "listField fieldContent emailField"
-                                placeholder = "Дополнительный E-mail"/>
-                            </div>
-                        </li>
-                    </ul>
-                    <div id = "mainPhone-layout" class = "companyField">
-                        <div id = "mainPhone" class = "fieldTitle">Основной телефон</div>
-                        <div class = "fieldBorder">
-                            <input v-model.lazy.trim = "contact.mainPhone"
-                            class = "fieldContent" placeholder = "Основной телефон" />
-                        </div>
-                    </div>
-                    <button id = "addPhone-button" class = "addButton-layout"
-                    v-on:click = "addPhoneButtonClick()">
-                        <div id = "addPhone-plus" class = "plus">+</div>
-                        <div id = "addPhone-text" class = "addButton-text">Добавить телефон</div>
-                    </button>
-                    <ul class = "list">
-                        <li class = "listElement" v-for = "phone in phones" v-bind:key = "phone">
-                            <div class = "fieldBorder">
-                                <input class = "listField fieldContent phoneField"
-                                placeholder = "Дополнительный телефон"/>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div id = "buttons-layout">
-                    <button id = "addContactButton-layout" v-on:click = "onAddButtonClick">
-                        <div id = "addContactButton-text">+ Добавить контактное лицо</div>
-                    </button>
-                </div>
+  <div>
+    <div id="main-layout">
+      <div id="addContact-layout">
+        <div id="addContact-header">
+          <router-link to="/kb" class="nav-element" active-class="active">
+            <div id="return-button">
+              <i class="fa fa-arrow-left" aria-hidden="true"></i>
             </div>
+          </router-link>
+          <div id="contactName-field">
+            <input
+              v-model.lazy.trim="contact.contactName"
+              id="contactName-input"
+              placeholder="ФИО"
+            />
+          </div>
         </div>
+        <div id="fields-layout">
+          <div id="companyId-layout" class="contactField">
+            <div id="companyId" class="fieldTitle">Компания</div>
+            <select v-model="contact.companyId" id="companyVolumeContent">
+              <option disabled value="0">Выберите компанию</option>
+              <option
+                v-for="company in companies"
+                v-bind:key="company.fullName"
+                v-bind:value="company.companyId"
+              >
+                {{ company.fullName }}
+              </option>
+            </select>
+          </div>
+          <div id="position-layout" class="companyField">
+            <div id="position" class="fieldTitle">Должность</div>
+            <div class="fieldBorder">
+              <input
+                v-model.lazy.trim="contact.position"
+                class="fieldContent"
+                placeholder="Должность"
+              />
+            </div>
+          </div>
+          <div id="makeDecision-layout" class="contactField">
+            <div id="makeDecision" class="fieldTitle">
+              Лицо принимающее решения
+            </div>
+            <input
+              v-model="contact.makeDecision"
+              type="checkbox"
+              class="fieldContent"
+              id="makeDecision-checkbox"
+            />
+          </div>
+          <div id="mainEmail-layout" class="companyField">
+            <div id="mainEmail" class="fieldTitle">Основной E-mail</div>
+            <div class="fieldBorder">
+              <input
+                v-model.lazy.trim="contact.mainEmail"
+                class="fieldContent"
+                placeholder="Основной E-mail"
+              />
+            </div>
+          </div>
+          <button id="addEmail-button" class="addButton-layout" v-on:click="addEmailButtonClick()">
+            <div id="addEmail-plus" class="plus">+</div>
+            <div id="addEmail-text" class="addButton-text">Добавить E-mail</div>
+          </button>
+          <ul class="list">
+            <li class="listElement" v-for="email in emails" v-bind:key="email">
+              <div class="fieldBorder">
+                <input
+                  class="listField fieldContent emailField"
+                  placeholder="Дополнительный E-mail"
+                />
+              </div>
+            </li>
+          </ul>
+          <div id="mainPhone-layout" class="companyField">
+            <div id="mainPhone" class="fieldTitle">Основной телефон</div>
+            <div class="fieldBorder">
+              <input
+                v-model.lazy.trim="contact.mainPhone"
+                class="fieldContent"
+                placeholder="Основной телефон"
+              />
+            </div>
+          </div>
+          <button id="addPhone-button" class="addButton-layout" v-on:click="addPhoneButtonClick()">
+            <div id="addPhone-plus" class="plus">+</div>
+            <div id="addPhone-text" class="addButton-text">Добавить телефон</div>
+          </button>
+          <ul class="list">
+            <li class="listElement" v-for="phone in phones" v-bind:key="phone">
+              <div class="fieldBorder">
+                <input
+                  class="listField fieldContent phoneField"
+                  placeholder="Дополнительный телефон"
+                />
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div id="buttons-layout">
+          <button id="addContactButton-layout" v-on:click="onAddButtonClick">
+            <div id="addContactButton-text">+ Добавить контактное лицо</div>
+          </button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import axios from 'axios';
-import store from '@/store';
 
 @Component
 export default class Contact extends Vue {
   addEmailButtonClick() {
     this.temp = '';
-
     this.emails.push('');
   }
 
   addPhoneButtonClick() {
     this.temp = '';
-
     this.phones.push('');
   }
 
   onAddButtonClick() {
-    const emailList = document.querySelectorAll('.emailField');
-    const phoneList = document.querySelectorAll('.phoneField');
+    const emailList = document.querySelectorAll<HTMLInputElement>('.emailField');
+    const phoneList = document.querySelectorAll<HTMLInputElement>('.phoneField');
     this.temp = '';
 
     emailList.forEach((email) => {
@@ -125,218 +142,237 @@ export default class Contact extends Vue {
       }
     });
 
-    store.dispatch('POST_NEW_CONTACT', [this.contact]);
+    this.$store
+      .dispatch('POST_NEW_CONTACT', [this.contact])
+      .then((response) => {
+        console.log(response);
+        this.$router.push('/kb');
+      })
+      .catch((error) => {
+        console.error(error);
+        this.$router.push('/kb');
+      });
   }
 
-    companies = store.getters.COMPANIES;
+  companies = this.$store.getters.COMPANIES;
 
-    mounted() {
-      this.$store.dispatch('GET_COMPANIES');
-    }
+  mounted() {
+    this.$store.dispatch('GET_COMPANIES');
+  }
 
-    temp = ''
+  temp = '';
 
-    emails = []
+  emails: string[] = [];
 
-    phones = []
+  phones: string[] = [];
 
-    contact = {
-      contactPersonId: null,
-      contactName: '',
-      companyId: 0,
-      position: '',
-      makeDecision: false,
-      mainEmail: '',
-      otherEmails: [],
-      creatorId: null,
-      lastUpdaterId: null,
-      mainPhone: null,
-      otherPhones: [],
-    }
+  contact = {
+    contactPersonId: null,
+    contactName: '',
+    companyId: 0,
+    position: '',
+    makeDecision: false,
+    mainEmail: '',
+    otherEmails: [] as string[],
+    creatorId: null,
+    lastUpdaterId: null,
+    mainPhone: null,
+    otherPhones: [] as string[],
+  };
 }
-
 </script>
 
 <style scoped lang="scss">
-
-::-webkit-input-placeholder {color:#BEBEBE; opacity:0.95;}/* webkit */
-::-moz-placeholder          {color:#BEBEBE; opacity:0.95;}/* Firefox 19+ */
-:-moz-placeholder           {color:#BEBEBE; opacity:0.95;}/* Firefox 18- */
-:-ms-input-placeholder      {color:#BEBEBE; opacity:0.95;}/* IE */
+::-webkit-input-placeholder {
+  color: #bebebe;
+  opacity: 0.95;
+} /* webkit */
+::-moz-placeholder {
+  color: #bebebe;
+  opacity: 0.95;
+} /* Firefox 19+ */
+:-moz-placeholder {
+  color: #bebebe;
+  opacity: 0.95;
+} /* Firefox 18- */
+:-ms-input-placeholder {
+  color: #bebebe;
+  opacity: 0.95;
+} /* IE */
 
 input::-webkit-outer-spin-button,
 input::-webkit-inner-spin-button {
-    -webkit-appearance: none;
+  -webkit-appearance: none;
 }
 
 #body {
-    text-align: center;
-    opacity: 0.95;
+  text-align: center;
+  opacity: 0.95;
 
-    #main-layout {
+  #main-layout {
+    display: inline-block;
+    margin-top: 25px;
+    padding-bottom: 25px;
+    box-shadow: 1.4px 1.4px 5px #707070;
+    border: 1px solid #ffffff;
+  }
+
+  #addContact-header {
+    text-align: left;
+    margin-top: 25px;
+    margin-left: 20px;
+    margin-right: 20px;
+    margin-bottom: 30px;
+    font-size: 28pt;
+    color: #7f7f7f;
+
+    #return-button {
+      display: inline-block;
+      color: #7f7f7f;
+
+      i:hover {
+        color: black;
+      }
+    }
+
+    #contactName-field {
+      display: inline-block;
+      margin-left: 20px;
+      margin-right: 20px;
+
+      #contactName-input {
         display: inline-block;
-        margin-top: 25px;
-        padding-bottom: 25px;
-        box-shadow: 1.4px 1.4px 5px #707070;
-        border: 1px solid #ffffff
-    }
-
-    #addContact-header {
-        text-align: left;
-        margin-top: 25px;
-        margin-left: 20px;
-        margin-right: 20px;
-        margin-bottom: 30px;
+        width: 600px;
         font-size: 28pt;
-        color: #7F7F7F;
+        border: white;
+        border-bottom: 1px solid #bebebe;
+        opacity: 0.9;
+        outline-style: none;
+      }
+    }
+  }
 
-        #return-button {
-            display: inline-block;
-            color: #7F7F7F;
+  #fields-layout {
+    display: grid;
+    grid-auto-columns: minmax(600px, auto);
+    text-align: left;
+    grid-row-gap: 10px;
+    margin-left: 20px;
+    margin-right: 20px;
 
-            i:hover {
-                color: black;
-            }
-        }
-
-        #contactName-field {
-            display: inline-block;
-            margin-left: 20px;
-            margin-right: 20px;
-
-            #contactName-input {
-                display: inline-block;
-                width: 600px;
-                font-size: 28pt;
-                border: white;
-                border-bottom: 1px solid #BEBEBE;
-                opacity: 0.9;
-                outline-style: none;
-            }
-        }
+    #makeDecision-checkbox {
+      margin-top: 5px;
+      transform: scale(1.5);
     }
 
-    #fields-layout {
-        display: grid;
-        grid-auto-columns: minmax(600px, auto);
-        text-align: left;
-        grid-row-gap: 10px;
-        margin-left: 20px;
-        margin-right: 20px;
-
-        #makeDecision-checkbox {
-            margin-top: 5px;
-            transform: scale(1.5);
-        }
-
-        .fieldTitle {
-            color: #7F7F7F;
-            font-size: 10pt;
-        }
-
-        .fieldBorder {
-            display: inline-block;
-            margin-top: 5px;
-            border-radius: 6px;
-            border: 1px solid #BEBEBE;
-
-            .fieldContent {
-                margin: 5px;
-                font-size: 14pt;
-                border: white;
-                outline-style: none;
-            }
-        }
-
-        .list {
-            padding-left: 0;
-            margin-left: 0;
-            margin-top: 0;
-            list-style: none;
-        }
-
-        .listElement {
-                border: 1px solid #ffffff;
-                margin-bottom: 5px;
-
-                .listField {
-                    border: 1px solid #ffffff;
-                    border-radius: 6px;
-                    outline-style: none;
-                }
-            }
-
-        #addEmail-button:hover {
-            #addEmail-plus{
-                color: #FFB300;
-            }
-        }
-
-        #addPhone-button:hover {
-            #addPhone-plus{
-                color: #FFB300;
-            }
-        }
-
-        .addButton-layout {
-            display: inline-block;
-            text-align: left;
-            width: 200px;
-            border: none;
-            outline: none;
-            background: white;
-
-            .plus {
-                display: inline-block;
-                font-size: 32pt;
-                margin-right: 10px;
-                margin-left: -5px;
-                color: #5AC37D;
-            }
-
-            .addButton-text {
-                display: inline-block;
-                vertical-align: top;
-                margin-top: 16px;
-                font-size: 12pt;
-                color:  #7F7F7F;
-            }
-        }
+    .fieldTitle {
+      color: #7f7f7f;
+      font-size: 10pt;
     }
 
-    #buttons-layout {
-        margin-top: 30px;
-        margin-right: 30px;
-        text-align: right;
+    .fieldBorder {
+      display: inline-block;
+      margin-top: 5px;
+      border-radius: 6px;
+      border: 1px solid #bebebe;
 
-        #addContactButton-layout {
-            display: inline-block;
-            border: 1px solid white;
-            padding: 10px;
-            border-radius: 12px;
-            opacity: 0.95;
-            text-decoration: none;
-            background: #5AC37D;
-
-            #addContactButton-text {
-                font-size: 14pt;
-                color: white;
-            }
-        }
-
-        #addContactButton-layout:active, #addContactButton-layout:focus {
-            outline: none;
-        }
-
-        #addContactButton-layout:hover {
-            border: 1px solid #508C64;
-            background: white;
-
-            #addContactButton-text {
-              color: #508C64;
-            }
-        }
+      .fieldContent {
+        margin: 5px;
+        font-size: 14pt;
+        border: white;
+        outline-style: none;
+      }
     }
+
+    .list {
+      padding-left: 0;
+      margin-left: 0;
+      margin-top: 0;
+      list-style: none;
+    }
+
+    .listElement {
+      border: 1px solid #ffffff;
+      margin-bottom: 5px;
+
+      .listField {
+        border: 1px solid #ffffff;
+        border-radius: 6px;
+        outline-style: none;
+      }
+    }
+
+    #addEmail-button:hover {
+      #addEmail-plus {
+        color: #ffb300;
+      }
+    }
+
+    #addPhone-button:hover {
+      #addPhone-plus {
+        color: #ffb300;
+      }
+    }
+
+    .addButton-layout {
+      display: inline-block;
+      text-align: left;
+      width: 200px;
+      border: none;
+      outline: none;
+      background: white;
+
+      .plus {
+        display: inline-block;
+        font-size: 32pt;
+        margin-right: 10px;
+        margin-left: -5px;
+        color: #5ac37d;
+      }
+
+      .addButton-text {
+        display: inline-block;
+        vertical-align: top;
+        margin-top: 16px;
+        font-size: 12pt;
+        color: #7f7f7f;
+      }
+    }
+  }
+
+  #buttons-layout {
+    margin-top: 30px;
+    margin-right: 30px;
+    text-align: right;
+
+    #addContactButton-layout {
+      display: inline-block;
+      border: 1px solid white;
+      padding: 10px;
+      border-radius: 12px;
+      opacity: 0.95;
+      text-decoration: none;
+      background: #5ac37d;
+
+      #addContactButton-text {
+        font-size: 14pt;
+        color: white;
+      }
+    }
+
+    #addContactButton-layout:active,
+    #addContactButton-layout:focus {
+      outline: none;
+    }
+
+    #addContactButton-layout:hover {
+      border: 1px solid #508c64;
+      background: white;
+
+      #addContactButton-text {
+        color: #508c64;
+      }
+    }
+  }
 }
-
 </style>
--- src/knowledge-base/components/ContactPersonList.vue
+++ src/knowledge-base/components/ContactPersonList.vue
@@ -1,64 +1,64 @@
 <template>
-    <div>
-        <ul id ="contactPersons-list">
-            <li class = "contact" v-for = "contact in contactPersons"
-            v-bind:key = "contact.contactName">
-                <div id = "contactName-layout" class = "contactField"
-                v-if = "contact.contactName != null">
-                    <div id = "contactName" class = "fieldContent">{{ contact.contactName }}</div>
-                </div>
-                <div v-for = "company in companies" v-bind:key = "company">
-                    <div id = "contactCompany-layout" class = "contactField"
-                    v-if = "contact.companyId == company.companyId">
-                        <div id = "contactCompany" class = "fieldTitle">Компания</div>
-                        <div class = "fieldContent">{{ company.fullName }}</div>
-                    </div>
-                </div>
-                <div id = "contactPosition-layout" class = "contactField"
-                v-if = "contact.position != ''">
-                    <div id = "contactPosition" class = "fieldTitle">Должность</div>
-                    <div class = "fieldContent">{{ contact.position }}</div>
-                </div>
-                <div v-if = "contact.makeDecision" id = "contactDecision-layout"
-                    class = "contactField">
-                    <div id = "makeDecisionField-text" class = "fieldContent">
-                        Лицо принимающее решения
-                    </div>
-                </div>
-                <div id = "contactEmail-layout" class = "contactField"
-                v-if = "contact.mainEmail != ''">
-                    <div id = "contactEmail" class = "fieldTitle">E-mail</div>
-                    <div class = "fieldContent">{{ contact.mainEmail }}</div>
-                </div>
-                <div id = "contactPhone-layout" class = "contactField"
-                v-if = "contact.mainPhone != null">
-                    <div id = "contactPhone" class = "fieldTitle">Телефон</div>
-                    <div class = "fieldContent">{{ contact.mainPhone }}</div>
-                </div>
-            </li>
-        </ul>
-    </div>
+  <div>
+    <ul id="contactPersons-list">
+      <li class="contact" v-for="contact in contactPersons" v-bind:key="contact.contactName">
+        <div id="contactName-layout" class="contactField" v-if="contact.contactName != null">
+          <div id="contactName" class="fieldContent">
+            {{ contact.contactName }}
+          </div>
+        </div>
+        <div v-for="company in companies" v-bind:key="company.fullName">
+          <div
+            id="contactCompany-layout"
+            class="contactField"
+            v-if="contact.companyId == company.companyId"
+          >
+            <div id="contactCompany" class="fieldTitle">Компания</div>
+            <div class="fieldContent">{{ company.fullName }}</div>
+          </div>
+        </div>
+        <div id="contactPosition-layout" class="contactField" v-if="contact.position != ''">
+          <div id="contactPosition" class="fieldTitle">Должность</div>
+          <div class="fieldContent">{{ contact.position }}</div>
+        </div>
+        <div v-if="contact.makeDecision" id="contactDecision-layout" class="contactField">
+          <div id="makeDecisionField-text" class="fieldContent">
+            Лицо принимающее решения
+          </div>
+        </div>
+        <div id="contactEmail-layout" class="contactField" v-if="contact.mainEmail != ''">
+          <div id="contactEmail" class="fieldTitle">E-mail</div>
+          <div class="fieldContent">{{ contact.mainEmail }}</div>
+        </div>
+        <div id="contactPhone-layout" class="contactField" v-if="contact.mainPhone != null">
+          <div id="contactPhone" class="fieldTitle">Телефон</div>
+          <div class="fieldContent">{{ contact.mainPhone }}</div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
-import store from '@/store';
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class ContactPersonList extends Vue {
-    contactPersons = store.getters.CONTACTS;
+  get contactPersons() {
+    return this.$store.getters.CONTACTS;
+  }
 
-    companies = store.getters.COMPANIES;
+  get companies() {
+    return this.$store.getters.COMPANIES;
+  }
 
-    mounted() {
-      this.$store.dispatch('GET_CONTACTS');
-    }
+  mounted() {
+    this.$store.dispatch('GET_CONTACTS');
+  }
 }
-
 </script>
 
 <style scoped lang="scss">
-
 #contactPersons-list {
   display: grid;
   grid-template-columns: auto auto;
@@ -67,63 +67,62 @@ export default class ContactPersonList extends Vue {
   text-align: left;
 
   .contact {
-        display: inline-block;
-        list-style: none;
-        vertical-align: top;
-        // width: 45%;
-        margin-top: 10px;
-        margin-right: 5px;
-        margin-left: 5px;
-        padding-top:10px;
-        padding-left: 5px;
-        padding-bottom: 10px;
-        box-shadow: 1.3px 1.3px 5px #707070;
-        border: 1px solid #ffffff
-        // background: yellow;
+    display: inline-block;
+    list-style: none;
+    vertical-align: top;
+    // width: 45%;
+    margin-top: 10px;
+    margin-right: 5px;
+    margin-left: 5px;
+    padding-top: 10px;
+    padding-left: 5px;
+    padding-bottom: 10px;
+    box-shadow: 1.3px 1.3px 5px #707070;
+    border: 1px solid #ffffff;
+    // background: yellow;
+  }
+
+  .contact:hover {
+    border-color: #508c64;
+  }
+
+  #contactName {
+    font-size: 24pt;
+    padding-bottom: 5px;
+  }
+
+  .contactField {
+    margin-top: 4px;
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-bottom: 5px;
+    // background: royalblue;
+  }
+
+  .fieldTitle {
+    font-size: 10pt;
+    color: #7f7f7f;
+  }
+
+  .fieldContent {
+    font-size: 14pt;
+    opacity: 0.95;
+  }
+
+  #contactDecision-layout {
+    display: inline-block;
+    padding-left: 10px;
+    padding-right: 10px;
+    padding-top: 2px;
+    padding-bottom: 2px;
+    border-radius: 12px;
+    opacity: 0.95;
+    background: #5ac37d;
+
+    #makeDecisionField-text {
+      font-size: 14pt;
+      color: white;
     }
-
-    .contact:hover {
-        border-color: #508C64;
-    }
-
-    #contactName {
-        font-size: 24pt;
-        padding-bottom: 5px;
-    }
-
-    .contactField {
-        margin-top: 4px;
-        margin-left: 10px;
-        margin-right: 10px;
-        margin-bottom: 5px;
-        // background: royalblue;
-    }
-
-    .fieldTitle {
-        font-size: 10pt;
-        color: #7F7F7F;
-    }
-
-    .fieldContent {
-        font-size: 14pt;
-        opacity: 0.95;
-    }
-
-    #contactDecision-layout {
-          display: inline-block;
-          padding-left: 10px;
-          padding-right: 10px;
-          padding-top: 2px;
-          padding-bottom: 2px;
-          border-radius: 12px;
-          opacity: 0.95;
-          background: #5AC37D;
-
-          #makeDecisionField-text {
-            font-size: 14pt;
-            color: white;
-          }
-        }
+  }
 }
-
 </style>
